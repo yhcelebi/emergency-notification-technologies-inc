d6ba054dfb366401cfae74184fee3ac23ec12fc4
Yusuf Hamza Çelebi <yhcelebi@gmail.com>
2023-10-15 18:00:31 +0300

    resolved Navbar.tsx errors on build

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,10 +18,10 @@ export function Navbar() {
 
   // set theme state in localstorage on mount & also update localstorage on state change
   useEffect(() => {
-    localStorage.setItem("theme", theme);
+    localStorage.setItem("theme", theme || "");
     const localTheme = localStorage.getItem("theme");
     // add custom data-theme attribute to html tag required to update theme using DaisyUI
-    document.querySelector("html").setAttribute("data-theme", localTheme);
+    document.querySelector("html")?.setAttribute("data-theme", localTheme || "");
   }, [theme]);
 
   return (
